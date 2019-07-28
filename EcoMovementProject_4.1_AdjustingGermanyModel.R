ae6239cecc2332a452eba0297ae9366aac0387e7
Alican Tanaçan <47318455+AlicanTanacan@users.noreply.github.com>
2019-07-28 10:42:40 +0200

    Add files via upload

---
+++ EcoMovementProject_4.1_AdjustingGermanyModel.R
@@ -0,0 +1,176 @@
+### ------------ Eco Movement Project ------------ ###
+### ------------- by Alican Tanaçan -------------- ###
+### ----- Version 4.1: Adjusting for Norway ------ ###
+
+### This version is only here for adjusting variables and their data types for 
+### predicting Norway data as a validation dataset.
+
+### ---- Libraries & Source ---- 
+if(require("pacman") == "FALSE"){
+  install.packages("pacman")
+}
+p_load(dplyr, ggplot2, plotly, caret, corrplot, GGally,
+       doParallel, tidyverse, e1071, randomForest, caTools,
+       plyr, ROSE, Hmisc, vcd, polycor, gbm)
+
+## Take data from version 1 with source
+source(file = "D:/RStudio/R Studio Working Directory/Eco Movement/EcoMovementProject_1_MergingData.R")
+
+### ---- Import Dataset ----
+OriginalEcoData <- read.csv("EcoData.csv", stringsAsFactors = F)
+
+### ---- Preprocessing & Feature Selection ----
+## Remove auto-generated column "X"
+OriginalEcoData$X <- NULL
+
+## Save Original Dataset
+EcoData <- OriginalEcoData
+
+## Missing Value Treatment
+EcoData[is.na(EcoData)] <- 0
+sum(is.na(EcoData)) # No NA's
+
+## Create a new variable that depicts open hours of chargers
+EcoData %>% 
+  mutate(
+    Open_Hours = 
+      if_else(opening_times %in% c("{\"twentyfourseven\": true }",
+                                   "{\"twentyfourseven\":true}"), "Everytime",
+              if_else(opening_times %in% "0", "Unknown",
+                      "Regular"))) -> EcoData
+
+## Change variable type
+EcoData$Open_Hours <- as.factor(EcoData$Open_Hours)
+
+## Remove Unknown Open Hours 
+EcoData %>% 
+  filter(Open_Hours != "Unknown") -> EcoData
+
+## Remove Nondescriptive, Irrelevant and Uninformative Variables
+EcoData %>%
+  select(power,
+         Open_Hours,
+         lat,
+         lng,
+         public_access_type_id) -> ReadyEcoData
+
+## Move Dependent Variable to end
+ReadyEcoData %>% 
+  select(-public_access_type_id, public_access_type_id) -> ReadyEcoData
+
+### ---- Sampling  ----
+## Amount of Observations in each level of y
+ReadyEcoData %>% 
+  group_by(public_access_type_id) %>% 
+  summarise(count(public_access_type_id))
+# 4596 Public
+# 77 Private
+# 1864 Company
+
+## Change y data type to character in order to subset without empty classes
+ReadyEcoData$public_access_type_id <- as.character(ReadyEcoData$public_access_type_id)
+
+## Divide the data into 3 subsets for the levels of y
+ReadyEcoData %>% 
+  filter(public_access_type_id == 1) -> Data_Public
+
+ReadyEcoData %>% 
+  filter(public_access_type_id == 2) -> Data_Private
+
+ReadyEcoData %>% 
+  filter(public_access_type_id == 3) -> Data_Company
+
+## combining private with company
+EcoData_Sample_Priv_Comp <- rbind(Data_Private,
+                                  Data_Company)
+
+## Over Sampling the Private Company Data
+set.seed(200)
+EcoData_Sample_PrivCompOver <- ovun.sample(public_access_type_id~., 
+                                           data = EcoData_Sample_Priv_Comp,
+                                           p = 0.5,
+                                           seed = 1, 
+                                           method = "over")$data
+
+## Take a random sample from public that is equal to amount of company observations
+Public_Sample2 <- Data_Public[sample(1:nrow(Data_Public), 1864, replace = F),]
+
+## Merge both public and company subsets.
+EcoData_Sample2 <- rbind(Public_Sample2,
+                         EcoData_Sample_PrivCompOver)
+
+## Change y data type to factor for data partition
+EcoData_Sample2$public_access_type_id <- as.factor(EcoData_Sample2$public_access_type_id)
+EcoData_Sample2$capability_remote_start_stop_capable <- as.factor(EcoData_Sample2$capability_remote_start_stop_capable)
+
+### ---- Data Partition ----
+intrain <- createDataPartition(y = EcoData_Sample2$public_access_type_id, 
+                               p = 0.7, 
+                               list = FALSE)
+
+EcoData_Train <- EcoData_Sample2[intrain,]
+EcoData_Test <- EcoData_Sample2[-intrain,]
+
+EcoData_Train %>% 
+  group_by(public_access_type_id) %>% 
+  summarise(count(public_access_type_id))
+
+### ---- Core Selection ----
+## Find how many cores are on your machine
+detectCores() # Result = 8
+
+## Create cluster with desired number of cores. If you have less than 8 cores do NOT run this code!
+cl <- makeCluster(4)
+
+## Register cluster
+registerDoParallel(cl)
+
+### ---- RF Modelization (Random Forest) ----
+set.seed(4567)
+## Set Train Control
+RFtrctrl <- trainControl(method = "repeatedcv",
+                         number = 5,
+                         repeats = 2,
+                         verboseIter = TRUE)
+
+RFmodel2 <- train(public_access_type_id ~ ., 
+                  EcoData_Train,
+                  method = "rf",
+                  trControl = RFtrctrl)
+
+RFmodel2
+
+plot(RFmodel2)
+
+## Most Important Variables
+plot(varImp(RFmodel2))
+# power
+# Open_Hours (Regular)
+# lat
+# lng
+# capability_remote_start_stop_capable (Y)
+
+## Predicton on Test set
+predRFmodel2 <- predict(RFmodel2, EcoData_Test)
+
+postResample(predRFmodel2, EcoData_Test$public_access_type_id) -> RFmodel2metrics
+
+RFmodel2metrics
+
+## Confusion Matrix
+RFConfMat2 <- confusionMatrix(predRFmodel2, EcoData_Test$public_access_type_id) 
+RFConfMat2
+#              Reference
+# Prediction   1   2   3
+#          1 531   0  12
+#          2   1 544   4
+#          3  27   0 543
+
+# Accuracy: 0.973
+# Kappa: 0.960
+
+## Save the model to disk
+save(RFmodel2, file = "EcoRandomForestModel.rda")
+
+## Stop Cluster
+stopCluster(cl)
---
+++ EcoMovementProject_4.2_GermanyModelValidationNorway.R
@@ -0,0 +1,174 @@
+### ------------ Eco Movement Project ------------ ###
+### ------------- by Alican Tanaçan -------------- ###
+### --- Version 4.2: Validation on Norway Data --- ###
+
+### ---- Libraries & Source ---- 
+if(require("pacman") == "FALSE"){
+  install.packages("pacman")
+}
+p_load(dplyr, ggplot2, plotly, caret, corrplot, GGally,
+       doParallel, tidyverse, e1071, randomForest, caTools,
+       plyr, ROSE, Hmisc, vcd, polycor, gbm)
+
+### ---- Import Datasets ----
+Norway_Charging_Stations <- read.csv("D:/RStudio/R Studio Working Directory/Eco Movement/charging_stations_NO.csv")
+Norway_Charging_Spots <- read.csv("D:/RStudio/R Studio Working Directory/Eco Movement/charging_spots_NO.csv")
+Norway_Spot_Connectors <- read.csv("D:/RStudio/R Studio Working Directory/Eco Movement/spot_connectors_NO.csv")
+Norway_Master_Locations <- read.csv("D:/RStudio/R Studio Working Directory/Eco Movement/masterlocations_NO.csv")
+Norway_Reverse_Geodata <- read.csv("D:/RStudio/R Studio Working Directory/Eco Movement/reverse_geodata_NO.csv")
+
+## Save Original Datasets
+df1 <- Norway_Charging_Stations
+df2 <- Norway_Charging_Spots
+df3 <- Norway_Spot_Connectors
+df4 <- Norway_Master_Locations
+df5 <- Norway_Reverse_Geodata
+
+### ---- Preprocess Before Merging ----
+
+## Detecting NA's at the Dependent Variables
+summary(df1$public_access_type_id) # There are 24 NA's in public_access_type_id
+
+## Exclude NA's from the Dependent Variable
+df1 <- df1[!is.na(df1$public_access_type_id),]
+
+## Check how many observations are there in Dependent Variable
+df1 %>% 
+  group_by(public_access_type_id) %>% 
+  summarise(count(public_access_type_id))
+# Public 1693
+# Private 391
+# Company 746
+
+## Removing Nondescriptive, Irrelevant and Uninformative Variables
+df1 %>% 
+  select(chargingstation_id,
+         masterlocation_id,
+         physical_type,
+         operator_id,
+         owner_id,
+         public_access_type_id,
+         opening_times,
+         charging_when_closed,
+         additional_geo_location) -> df1
+
+df2 %>% 
+  select(chargingstation_id,
+         charging_spots_id,
+         realtime_status_available,
+         spot_status,
+         capability_remote_start_stop_capable,
+         capability_rfid_reader) -> df2
+
+df3 %>% 
+  select(charging_spots_id,
+         src_id,
+         connector_types_id,
+         connector_format,
+         powertype,
+         voltage,
+         amperage,
+         power) -> df3
+
+df4 %>% 
+  select(masterlocation_id,
+         name,
+         published,
+         point) -> df4
+
+df5 %>% 
+  select(masterlocation_id,
+         street,
+         postal_code,
+         city,
+         administrative_area_1,
+         administrative_area_2,
+         lat,
+         lng) -> df5
+
+### ---- Merge Datasets ----
+df12 <- merge(df1, df2, by = "chargingstation_id")
+
+df123 <- merge(df12, df3, by = "charging_spots_id")
+
+df1234 <- merge(df123, df4, by = "masterlocation_id")
+
+EcoDataNorway <- merge(df1234, df5, by = "masterlocation_id")
+
+## Save the Dataset
+write.csv(EcoDataNorway, file = "EcoDataNorway.csv")
+
+### --- Import Original Norway Data ----
+OriginalNorwayEcoData <- read.csv("EcoDataNorway.csv", stringsAsFactors = F)
+
+### ---- Preprocessing & Feature Selection ----
+## Remove auto-generated column "X"
+OriginalNorwayEcoData$X <- NULL
+
+## Save Original Dataset
+NorwayEcoData <- OriginalNorwayEcoData
+
+## Missing Value Treatment
+NorwayEcoData[is.na(NorwayEcoData)] <- 0
+sum(is.na(NorwayEcoData)) # No NA's
+
+## Check levels of opening_times
+levels(as.factor(EcoData$opening_times))
+
+## Create a new variable that depicts open hours of chargers
+NorwayEcoData %>% 
+  mutate(
+    Open_Hours = 
+      if_else(opening_times %in% c("{\"twentyfourseven\": true }",
+                                   "{\"twentyfourseven\":true}"), "Everytime",
+              if_else(opening_times %in% "0", "Unknown",
+                      "Regular"))) -> NorwayEcoData
+
+## Change variable type
+NorwayEcoData$Open_Hours <- as.factor(NorwayEcoData$Open_Hours)
+
+## Remove Unknown Open Hours 
+NorwayEcoData %>% 
+  filter(Open_Hours != "Unknown") -> NorwayEcoData
+
+## Remove Nondescriptive, Irrelevant and Uninformative Variables
+NorwayEcoData %>%
+  select(power,
+         Open_Hours,
+         lat,
+         lng,
+         public_access_type_id) -> ReadyNorwayEcoData
+
+## Move Dependent Variable to end
+ReadyNorwayEcoData %>% 
+  select(-public_access_type_id, public_access_type_id) -> ReadyNorwayEcoData
+
+## Change Data Types for Modelization
+str(ReadyNorwayEcoData)
+ReadyNorwayEcoData %>% 
+  mutate_at(c("Open_Hours",
+              "public_access_type_id"), as.factor) -> ReadyNorwayEcoData
+
+### ---- Predicting y with Germany Random Forest Model ----
+GermanyRFModel <- load("EcoRandomForestModel.rda")
+
+Pred_RF_Norway <- predict(RFmodel2, ReadyNorwayEcoData)
+
+postResample(Pred_RF_Norway, ReadyNorwayEcoData$public_access_type_id) -> NorwayRFMetrics
+
+NorwayRFMetrics
+
+## Confusion Matrix
+NorwayRFConfMat <- confusionMatrix(Pred_RF_Norway, ReadyNorwayEcoData$public_access_type_id) 
+NorwayRFConfMat
+
+# Accuracy : 0.553
+# Kappa : 0.0263 
+
+## The model that predicts Access Type (Charger Type) for Germany is not
+## good enough to predict charger types in Germany. It is because of lat 
+## and lng. We either have to combine all datasets to find a unified model
+## that predicts both countries or we have to create different models for 
+## each country. Now that we have a model for Germany, let's analyse Norway!
+
+## Important Note: This does NOT mean that Germany Model is inefficient!
