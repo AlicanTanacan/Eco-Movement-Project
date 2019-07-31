24966256c1d90f89bd43d375d6d6bb4f06c6d873
Alican Tanaçan <47318455+AlicanTanacan@users.noreply.github.com>
2019-07-31 09:09:54 +0200

    Add files via upload

---
+++ EcoMovementProject_8_ImprovingModelization.R
@@ -0,0 +1,111 @@
+### ------------ Eco Movement Project ------------ ###
+### ------------- by Alican Tanaçan -------------- ###
+### ----- Version 8: Improving Modelization ------ ###
+
+## Important Note: Please be careful to core selection, if your
+## computer does not have 8 cores, please do NOT run the core selection
+## cluster codes.
+
+### ---- Libraries ---- 
+if(require("pacman") == "FALSE"){
+  install.packages("pacman")
+}
+p_load(dplyr, ggplot2, plotly, caret, corrplot, GGally,
+       doParallel, tidyverse, e1071, randomForest, caTools,
+       plyr, ROSE, Hmisc, vcd, polycor, gbm)
+
+### --- Import Clean Norway Data ----
+CleanNorwayData <- readRDS(file = "CleanNorwayData.rds")
+
+### ---- Feature Selection ----
+CleanNorwayData %>% 
+  select(Open_Hours,
+         physical_type,
+         connector_format,
+         power,
+         capability_rfid_reader,
+         powertype,
+         charging_when_closed,
+         lat,
+         lng,
+         amperage,
+         Access_Type) -> ReadyNorwayData
+
+### ---- Data Partition ----
+## Check how many observations are there in Dependent Variable
+ReadyNorwayData %>% 
+  group_by(Access_Type) %>% 
+  summarise(count(Access_Type))
+# Public 2454
+# Private 451
+# Company 809
+
+intrain <- createDataPartition(y = CleanNorwayData$Access_Type, 
+                               p = 0.7, 
+                               list = FALSE)
+
+NorwayData_Train <- CleanNorwayData[intrain,]
+NorwayData_Test <- CleanNorwayData[-intrain,]
+
+NorwayData_Train %>% 
+  group_by(Access_Type) %>% 
+  summarise(count(Access_Type))
+# Public 1718
+# Private 316
+# Company 567
+
+### ---- Core Selection ----
+## Find how many cores are on your machine
+detectCores() # Result = 8
+
+## Create cluster with desired number of cores. If you have less than 8 cores do NOT run this code!
+cl <- makeCluster(4)
+
+## Register cluster
+registerDoParallel(cl)
+
+## Confirm how many cores are now assigned to R & RStudio
+getDoParWorkers() # Result = 4
+
+### ---- RF Modelization (Random Forest) ----
+set.seed(4567)
+## Set Train Control
+RFtrctrl <- trainControl(method = "repeatedcv",
+                         number = 5,
+                         repeats = 2,
+                         verboseIter = TRUE)
+
+RFmodel2 <- train(Access_Type ~ ., 
+                  NorwayData_Train,
+                  method = "rf",
+                  trControl = RFtrctrl)
+
+RFmodel2
+
+plot(RFmodel2)
+
+## Most Important Variables
+plot(varImp(RFmodel2))
+
+## Predicton on Test set
+predRFmodel2 <- predict(RFmodel2, NorwayData_Test)
+
+postResample(predRFmodel2, NorwayData_Test$Access_Type) -> RFmodel2metrics
+
+RFmodel2metrics
+
+## Confusion Matrix
+RFConfMat2 <- confusionMatrix(predRFmodel2, NorwayData_Test$Access_Type) 
+RFConfMat2
+#                   Reference
+# Prediction Company Private Public
+# Company     146      37     28
+# Private      17      51     16
+# Public       79      47    692
+
+# Accuracy: 0.798
+# Kappa: 0.565
+
+# Our model fails to predict company and instead predicts public.
+# Our model also fails to predict private because of few private observations in the data.
+# Out model predicts public very well with minor errors.
